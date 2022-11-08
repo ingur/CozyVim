fc2c56c341f801aba30ca36cbd60cc8bab1c8799
ingur <ingurv99@gmail.com>
2022-11-08 19:59:12 +0100

    formatting

--- lua/configs/alpha.lua
+++ lua/configs/alpha.lua
@@ -21,12 +21,12 @@ dashboard.section.buttons.val = {
 }
 
 local function footer()
-    local total_plugins = #vim.tbl_keys(packer_plugins)
+    local total_plugins = "   " .. #vim.tbl_keys(packer_plugins) .. " plugins"
     local datetime = os.date(" %d-%m-%Y   %H:%M:%S")
     local version = vim.version()
     local nvim_version_info = "  ❤ v" .. version.major .. "." .. version.minor .. "." .. version.patch
 
-    return datetime .. "   " .. total_plugins .. " plugins" .. nvim_version_info
+    return datetime .. total_plugins .. nvim_version_info
 end
 
 dashboard.section.footer.val = footer()
