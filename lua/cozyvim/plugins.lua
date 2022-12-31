64bdcd3e524aba36d6020e9454da3968cacbfb47
ingur <ingurv99@gmail.com>
2022-12-31 03:29:42 +0100

    added tokynight theme option (and stylua formatting oops)

--- lua/cozyvim/plugins.lua
+++ lua/cozyvim/plugins.lua
@@ -1,286 +1,299 @@
 local plugins = {
-    { "nvim-lua/plenary.nvim" },
+	{ "nvim-lua/plenary.nvim" },
 
-    { "kyazdani42/nvim-web-devicons" },
+	{ "kyazdani42/nvim-web-devicons" },
 
-    { "folke/which-key.nvim" },
+	{ "folke/which-key.nvim" },
 
-    {
-        "dstein64/vim-startuptime",
-        cmd = "StartupTime",
-        config = function()
-            vim.g.startuptime_tries = 10
-        end,
-    },
+	{
+		"dstein64/vim-startuptime",
+		cmd = "StartupTime",
+		config = function()
+			vim.g.startuptime_tries = 10
+		end,
+	},
 
-    { "nvim-lualine/lualine.nvim", event = "VeryLazy", config = true },
+	{ "nvim-lualine/lualine.nvim", event = "VeryLazy", config = true },
 
-    { "nmac427/guess-indent.nvim", cmd = { "GuessIndent" }, config = true },
+	{ "nmac427/guess-indent.nvim", cmd = { "GuessIndent" }, config = true },
 
-    { "gpanders/editorconfig.nvim" },
+	{ "gpanders/editorconfig.nvim" },
 
-    {
-        "folke/trouble.nvim",
-        cmd = { "TroubleToggle", "Trouble" },
-        config = {
-            auto_open = false,
-            use_diagnostic_signs = true, -- en
-        },
-    },
+	{
+		"folke/trouble.nvim",
+		cmd = { "TroubleToggle", "Trouble" },
+		config = {
+			auto_open = false,
+			use_diagnostic_signs = true, -- en
+		},
+	},
 
-    {
-        "folke/persistence.nvim",
-        event = "BufReadPre",
-        config = { options = { "buffers", "curdir", "tabpages", "winsize", "help" } },
-    },
+	{
+		"folke/persistence.nvim",
+		event = "BufReadPre",
+		config = { options = { "buffers", "curdir", "tabpages", "winsize", "help" } },
+	},
 
-    { "lukas-reineke/virt-column.nvim", event = "BufRead", config = true },
+	{ "lukas-reineke/virt-column.nvim", event = "BufRead", config = true },
 
-    {
-        "lukas-reineke/indent-blankline.nvim",
-        event = "BufRead",
-        config = function()
-            vim.opt.list = true
-            vim.opt.listchars:append("eol:↴")
-            require("indent_blankline").setup({
-                show_end_of_line = true,
-            })
-        end,
-    },
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		event = "BufRead",
+		config = function()
+			vim.opt.list = true
+			vim.opt.listchars:append("eol:↴")
+			require("indent_blankline").setup({
+				show_end_of_line = true,
+			})
+		end,
+	},
 
-    {
-        "karb94/neoscroll.nvim",
-        -- these are the default keys used by neoscroll
-        keys = { "<C-u>", "<C-d>", "<C-b>", "<C-f>", "<C-y>", "<C-e>", "zt", "zz", "zb" },
-        config = true,
-    },
+	{
+		"karb94/neoscroll.nvim",
+		-- these are the default keys used by neoscroll
+		keys = { "<C-u>", "<C-d>", "<C-b>", "<C-f>", "<C-y>", "<C-e>", "zt", "zz", "zb" },
+		config = true,
+	},
 
-    { "lewis6991/gitsigns.nvim", event = "BufRead", config = true },
+	{ "lewis6991/gitsigns.nvim", event = "BufRead", config = true },
 
-    {
-        "gelguy/wilder.nvim",
-        event = "CmdlineEnter",
-        dependencies = { "romgrk/fzy-lua-native" },
-        config = function()
-            require("cozyvim.core.wilder")
-        end,
-    },
+	{
+		"gelguy/wilder.nvim",
+		event = "CmdlineEnter",
+		dependencies = { "romgrk/fzy-lua-native" },
+		config = function()
+			require("cozyvim.core.wilder")
+		end,
+	},
 
-    -- comments
+	-- comments
 
-    { "numToStr/Comment.nvim", keys = { "gc", "gb" }, config = true },
+	{ "numToStr/Comment.nvim", keys = { "gc", "gb" }, config = true },
 
-    {
-        "folke/todo-comments.nvim",
-        cmd = { "TodoTrouble", "TodoTelescope" },
-        event = "BufReadPost",
-        keys = { "]t", "[t" },
-        config = true,
-    },
+	{
+		"folke/todo-comments.nvim",
+		cmd = { "TodoTrouble", "TodoTelescope" },
+		event = "BufReadPost",
+		keys = { "]t", "[t" },
+		config = true,
+	},
 
-    -- terminal plugins
+	-- terminal plugins
 
-    {
-        "akinsho/toggleterm.nvim",
-        version = "*",
-        event = "BufWinEnter",
-        config = function()
-            require("cozyvim.core.toggleterm")
-        end,
-    },
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		event = "BufWinEnter",
+		config = function()
+			require("cozyvim.core.toggleterm")
+		end,
+	},
 
-    -- telescope plugins
+	-- telescope plugins
 
-    {
-        "nvim-telescope/telescope.nvim",
-        cmd = "Telescope",
-        config = function()
-            require("cozyvim.core.telescope")
-        end,
-    },
+	{
+		"nvim-telescope/telescope.nvim",
+		cmd = "Telescope",
+		config = function()
+			require("cozyvim.core.telescope")
+		end,
+	},
 
-    { "nvim-telescope/telescope-ui-select.nvim" },
+	{ "nvim-telescope/telescope-ui-select.nvim" },
 
-    { "nvim-telescope/telescope-file-browser.nvim" },
+	{ "nvim-telescope/telescope-file-browser.nvim" },
 
-    { "nvim-telescope/telescope-project.nvim", event = "BufWinEnter" },
+	{ "nvim-telescope/telescope-project.nvim", event = "BufWinEnter" },
 
-    -- navigation plugins
+	-- navigation plugins
 
-    {
-        "nacro90/numb.nvim",
-        event = "BufRead",
-        config = function()
-            require("numb").setup({
-                show_numbers = true, -- Enable "number" for the window while peeking
-                show_cursorline = true, -- Enable "cursorline" for the window while peeking
-            })
-        end,
-    },
+	{
+		"nacro90/numb.nvim",
+		event = "BufRead",
+		config = function()
+			require("numb").setup({
+				show_numbers = true, -- Enable "number" for the window while peeking
+				show_cursorline = true, -- Enable "cursorline" for the window while peeking
+			})
+		end,
+	},
 
-    { "phaazon/hop.nvim", event = "BufRead", config = true },
+	{ "phaazon/hop.nvim", event = "BufRead", config = true },
 
-    {
-        "akinsho/bufferline.nvim",
-        event = "BufWinEnter",
-        branch = "main",
-        config = function()
-            require("cozyvim.core.bufferline")
-        end,
-    },
+	{
+		"akinsho/bufferline.nvim",
+		event = "BufWinEnter",
+		branch = "main",
+		config = function()
+			require("cozyvim.core.bufferline")
+		end,
+	},
 
-    -- treesitter plugins
+	-- treesitter plugins
 
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        event = "BufReadPost",
-        config = function()
-            require("cozyvim.core.treesitter")
-        end,
-    },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		event = "BufReadPost",
+		config = function()
+			require("cozyvim.core.treesitter")
+		end,
+	},
 
-    { "nvim-treesitter/nvim-treesitter-refactor" },
+	{ "nvim-treesitter/nvim-treesitter-refactor" },
 
-    {
-        "nvim-treesitter/nvim-treesitter-context",
-        config = function()
-            require("treesitter-context").setup({})
-        end,
-    },
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		config = function()
+			require("treesitter-context").setup({})
+		end,
+	},
 
-    { "p00f/nvim-ts-rainbow" },
+	{ "p00f/nvim-ts-rainbow" },
 
-    {
-        "andymass/vim-matchup",
-        event = "BufReadPost",
-        config = function()
-            vim.g.matchup_matchparen_offscreen = { method = "popup" }
-        end,
-    },
+	{
+		"andymass/vim-matchup",
+		event = "BufReadPost",
+		config = function()
+			vim.g.matchup_matchparen_offscreen = { method = "popup" }
+		end,
+	},
 
-    -- lsp and autocompletion plugins
+	-- lsp and autocompletion plugins
 
-    { "williamboman/mason-lspconfig.nvim" },
+	{ "williamboman/mason-lspconfig.nvim" },
 
-    { "jay-babu/mason-null-ls.nvim", commit = "ab5d99619de2263508abb7fb05ef3a0f24a8d73d" },
+	{ "jay-babu/mason-null-ls.nvim", commit = "ab5d99619de2263508abb7fb05ef3a0f24a8d73d" },
 
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("cozyvim.core.mason")
-        end,
-    },
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("cozyvim.core.mason")
+		end,
+	},
 
-    {
-        "neovim/nvim-lspconfig",
-        event = "BufReadPre",
-        dependencies = { "hrsh7th/cmp-nvim-lsp" },
-        config = function()
-            require("cozyvim.core.lsp")
-        end,
-    },
+	{
+		"neovim/nvim-lspconfig",
+		event = "BufReadPre",
+		dependencies = { "hrsh7th/cmp-nvim-lsp" },
+		config = function()
+			require("cozyvim.core.lsp")
+		end,
+	},
 
-    { "lukas-reineke/lsp-format.nvim" },
+	{ "lukas-reineke/lsp-format.nvim" },
 
-    { "j-hui/fidget.nvim", event = "BufReadPre", config = true },
+	{ "j-hui/fidget.nvim", event = "BufReadPre", config = true },
 
-    {
-        "hrsh7th/nvim-cmp",
-        event = { "InsertEnter", "CursorHold" },
-        dependencies = {
-            "hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/cmp-buffer",
-            "hrsh7th/nvim-cmp",
-            "onsails/lspkind.nvim", -- vscode-like pictograms
-            {
-                "zbirenbaum/copilot-cmp",
-                config = function()
-                    require("copilot_cmp").setup()
-                end,
-            },
-            "saadparwaiz1/cmp_luasnip",
-            "L3MON4D3/LuaSnip",
-        },
-        config = function()
-            require("cozyvim.core.cmp")
-        end,
-    },
+	{
+		"hrsh7th/nvim-cmp",
+		event = { "InsertEnter", "CursorHold" },
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/nvim-cmp",
+			"onsails/lspkind.nvim", -- vscode-like pictograms
+			{
+				"zbirenbaum/copilot-cmp",
+				config = function()
+					require("copilot_cmp").setup()
+				end,
+			},
+			"saadparwaiz1/cmp_luasnip",
+			"L3MON4D3/LuaSnip",
+		},
+		config = function()
+			require("cozyvim.core.cmp")
+		end,
+	},
 
-    { "jose-elias-alvarez/null-ls.nvim" },
+	{ "jose-elias-alvarez/null-ls.nvim" },
 
-    {
-        "zbirenbaum/copilot.lua",
-        event = "VimEnter",
-        config = function()
-            vim.defer_fn(function()
-                require("copilot").setup()
-            end, 100)
-        end,
-        enabled = cozyvim.copilot.enabled,
-    },
+	{
+		"zbirenbaum/copilot.lua",
+		event = "VimEnter",
+		config = function()
+			vim.defer_fn(function()
+				require("copilot").setup()
+			end, 100)
+		end,
+		enabled = cozyvim.copilot.enabled,
+	},
 
-    { "windwp/nvim-autopairs", config = true },
+	{ "windwp/nvim-autopairs", config = true },
 
-    { "rmagatti/goto-preview", config = {
-        default_mappings = false,
-    } },
+	{ "rmagatti/goto-preview", config = {
+		default_mappings = false,
+	} },
 
-    -- startup screen
+	-- startup screen
 
-    {
-        "goolord/alpha-nvim",
-        lazy = false,
-        config = function()
-            require("cozyvim.core.alpha")
-        end,
-    },
+	{
+		"goolord/alpha-nvim",
+		lazy = false,
+		config = function()
+			require("cozyvim.core.alpha")
+		end,
+	},
 
-    -- colorthemes
+	-- colorthemes
 
-    {
-        "sainnhe/gruvbox-material",
-        lazy = false,
-        priority = 1000,
-        config = function()
-            vim.opt.background = "dark"
-            vim.g.gruvbox_material_better_performance = 1
-            vim.cmd("colorscheme " .. cozyvim.colorscheme)
-        end,
-        enabled = cozyvim.colorscheme == "gruvbox-material",
-    },
+	{
+		"sainnhe/gruvbox-material",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.opt.background = "dark"
+			vim.g.gruvbox_material_better_performance = 1
+			vim.cmd("colorscheme " .. cozyvim.colorscheme)
+		end,
+		enabled = cozyvim.colorscheme == "gruvbox-material",
+	},
 
-    {
-        "sainnhe/everforest",
-        lazy = false,
-        priority = 1000,
-        config = function()
-            vim.opt.background = "dark"
-            vim.g.everforest_better_performance = 1
-            vim.cmd("colorscheme " .. cozyvim.colorscheme)
-        end,
-        enabled = cozyvim.colorscheme == "everforest",
-    },
+	{
+		"sainnhe/everforest",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.opt.background = "dark"
+			vim.g.everforest_better_performance = 1
+			vim.cmd("colorscheme " .. cozyvim.colorscheme)
+		end,
+		enabled = cozyvim.colorscheme == "everforest",
+	},
 
-    {
-        "olimorris/onedarkpro.nvim",
-        lazy = false,
-        priority = 1000,
-        config = function()
-            vim.cmd("colorscheme " .. cozyvim.colorscheme)
-        end,
-        enabled = vim.tbl_contains({ "onedark", "onelight", "onedark_vivid", "onedark_dark" }, cozyvim.colorscheme),
-    },
+	{
+		"olimorris/onedarkpro.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd("colorscheme " .. cozyvim.colorscheme)
+		end,
+		enabled = vim.tbl_contains({ "onedark", "onelight", "onedark_vivid", "onedark_dark" }, cozyvim.colorscheme),
+	},
 
-    {
-        "rmehri01/onenord.nvim",
-        lazy = false,
-        priority = 1000,
-        config = function()
-            vim.cmd("colorscheme " .. cozyvim.colorscheme)
-        end,
-        enabled = cozyvim.colorscheme == "onenord",
-    },
+	{
+		"rmehri01/onenord.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd("colorscheme " .. cozyvim.colorscheme)
+		end,
+		enabled = cozyvim.colorscheme == "onenord",
+	},
+
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd("colorscheme " .. cozyvim.colorscheme)
+		end,
+		enabled = vim.tbl_contains(
+			{ "tokyonight", "tokyonight-night", "tokyonight-storm", "tokyonight-day", "tokyonight-moon" },
+			cozyvim.colorscheme
+		),
+	},
 }
 
 return plugins
