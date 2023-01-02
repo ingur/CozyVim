410ded42b41536bce7ccbd0b22b25b75b6773395
ingur <ingurv99@gmail.com>
2023-01-02 18:23:51 +0100

    disabled less rtp plugins

--- lua/cozyvim/lazy.lua
+++ lua/cozyvim/lazy.lua
@@ -3,46 +3,23 @@ plugins = vim.tbl_deep_extend("force", plugins, cozyvim.add_keys(require("custom
 plugins = vim.tbl_values(plugins)
 
 local options = {
-    defaults = { lazy = true },
-    install = { colorscheme = { "habamax" } },
-    -- checker = { enabled = true },
-    performance = {
-        rtp = {
-            disabled_plugins = {
-                "2html_plugin",
-                "getscript",
-                "getscriptPlugin",
-                "gzip",
-                "logipat",
-                "netrw",
-                "netrwPlugin",
-                "netrwSettings",
-                "netrwFileHandlers",
-                "matchit",
-                "tar",
-                "tarPlugin",
-                "rrhelper",
-                "spellfile_plugin",
-                "vimball",
-                "vimballPlugin",
-                "zip",
-                "zipPlugin",
-                "python3_provider",
-                "python_provider",
-                "node_provider",
-                "ruby_provider",
-                "perl_provider",
-                "tutor",
-                "rplugin",
-                "syntax",
-                "synmenu",
-                "optwin",
-                "compiler",
-                "bugreport",
-                "ftplugin",
-            }
-        }
-    }
+	defaults = { lazy = true },
+	install = { colorscheme = { "habamax" } },
+	-- checker = { enabled = true },
+	performance = {
+		rtp = {
+			disabled_plugins = {
+				"gzip",
+				"matchit",
+				"matchparen",
+				"netrwPlugin",
+				"tarPlugin",
+				"tohtml",
+				"tutor",
+				"zipPlugin",
+			},
+		},
+	},
 }
 
 require("lazy").setup(plugins, options)
