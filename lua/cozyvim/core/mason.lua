3b5fa981375e67999c5edf9784fc82d7311c8799
ingur <ingurv99@gmail.com>
2023-02-12 16:08:51 +0100

    prepare for upgrade

--- lua/cozyvim/core/mason.lua
+++
@@ -1,8 +0,0 @@
-local mason = require("mason")
-local mason_lspconfig = require("mason-lspconfig")
-
-mason.setup()
-
-mason_lspconfig.setup({
-    automatic_installation = true,
-})
--- lua/cozyvim/core/telescope.lua
+++
@@ -1,9 +0,0 @@
-local telescope = require("telescope")
-
-telescope.setup({})
-
-telescope.load_extension("ui-select")
-
-telescope.load_extension("file_browser")
-
-telescope.load_extension("project")
