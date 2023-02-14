fd2965c1c83cb96fa783142685704efd990f565a
ingur <ingurv99@gmail.com>
2023-02-14 02:58:57 +0100

    initial test

---
+++ lua/cozyvim/updater.lua
@@ -0,0 +1,19 @@
+local job = require("plenary.job")
+
+job:new({
+    command = "git",
+    args = { "fetch", "--dry-run" },
+    cwd = vim.fn.stdpath("config"),
+    on_stderr = function(_, data)
+        -- git reports progress to error stream
+        if data == nil then return end
+        job:new({
+            command = "git",
+            args = { "pull" },
+            cwd = vim.fn.stdpath("config"),
+            on_stderr = function(_, data2)
+                print(data2)
+            end,
+        }):start()
+    end,
+}):start()
