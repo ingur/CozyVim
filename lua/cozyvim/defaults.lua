bbdb0329c2a8cdfd1672181149ba9988870876d3
ingur <ingurv99@gmail.com>
2023-02-16 18:57:27 +0100

    removed duplicate copilot oops

--- lua/cozyvim/defaults.lua
+++ lua/cozyvim/defaults.lua
@@ -1,10 +1,6 @@
 cozyvim = {
     colorscheme = "gruvbox-material",
     updater = true, -- automatically keep CozyVim up to date
-    copilot = { -- GitHub Copilot settings
-        enabled = false,
-        cmp = false,
-    },
     nls = { -- null-ls settings
         sources = {
             code_actions = {},
