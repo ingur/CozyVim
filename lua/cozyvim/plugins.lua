ca6767f6523b2ad955f6ff46e34e2359584beb63
ingur <ingurv99@gmail.com>
2023-02-28 01:10:42 +0100

    improved indent-blankline

--- lua/cozyvim/plugins.lua
+++ lua/cozyvim/plugins.lua
@@ -330,8 +330,10 @@ return {
 
     {
         "lukas-reineke/indent-blankline.nvim",
-        event = "bufread",
+        event = { "BufReadPost", "BufNewFile" },
         opts = {
+            filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
+            show_trailing_blankline_indent = false,
             show_current_context = true,
             show_end_of_line = true,
         },
