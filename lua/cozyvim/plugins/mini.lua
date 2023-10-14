8e1113c90d1c50f7dfb0c86f32673ce731948427
ingur <ingurv99@gmail.com>
2023-10-14 19:03:59 +0200

    added simple MiniBufremove binds
    - might need to set these to a different submenu, like <leader>wq instead of qw in the future

--- lua/cozyvim/plugins/mini.lua
+++ lua/cozyvim/plugins/mini.lua
@@ -2,6 +2,10 @@ return {
 	"echasnovski/mini.nvim",
 	version = false,
 	lazy = false,
+	keys = {
+		{ "<leader>qw", "<cmd>lua MiniBufremove.delete()<cr>",        desc = "Delete buffer" },
+		{ "<leader>qW", "<cmd>lua MiniBufremove.delete(0, true)<cr>", desc = "Delete buffer (force)" },
+	},
 	config = function()
 		local function lazyload(module, config)
 			vim.api.nvim_create_autocmd("User", {
@@ -29,6 +33,6 @@ return {
 
 		lazyload("mini.cursorword", {}) -- highlight word on cursor
 		lazyload("mini.bufremove", {}) -- easy close buffers
-		lazyload("mini.move", {}) -- move lines/selections with alt
+		lazyload("mini.move", {})     -- move lines/selections with alt
 	end,
 }
