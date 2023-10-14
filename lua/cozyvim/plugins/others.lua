43b980b014188b42898b7038af0b225b76ee37d6
ingur <ingurv99@gmail.com>
2023-10-14 18:55:53 +0200

    javascript conform.nvim base setup added

--- lua/cozyvim/plugins/others.lua
+++ lua/cozyvim/plugins/others.lua
@@ -51,6 +51,7 @@ return {
 			formatters_by_ft = {
 				lua = { "stylua" },
 				python = { "isort", "black" },
+				javascript = { { "prettierd", "prettier" } },
 			},
 			format_on_save = {
 				timeout_ms = 500,
