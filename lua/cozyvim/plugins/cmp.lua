4fe385f4affb812d334d8b36b56e9cea2f928144
ingurv <ingurv99@gmail.com>
2024-01-14 17:17:27 +0100

    added c-j and c-k commands for jumping snippets insert

--- lua/cozyvim/plugins/cmp.lua
+++ lua/cozyvim/plugins/cmp.lua
@@ -147,6 +147,20 @@ return {
 							fallback()
 						end
 					end, { "i", "s" }),
+					["<C-j>"] = cmp.mapping(function(fallback)
+						if luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+					["<C-k>"] = cmp.mapping(function(fallback)
+						if luasnip.jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
 				}),
 			})
 
